8172be1a7b9a30d1c749673a3f8fded14d149dcf
jaydenshi88 <jaydenshi415@gmail.com>
2024-09-22 14:05:55 -0600

    dfd

--- Romans.java
+++ Romans.java
@@ -4,20 +4,39 @@
 
 import java.util.*;
 
-public class Romans
-{
+public class Romans {
 	private Map<String, Integer> m;
-	
-	public Romans()
-	{
+
+	public Romans() {
 		m = new TreeMap<String, Integer>();
-		
-		//complete your map				
+
+		m.put("I", 1);
+		m.put("V", 5);
+		m.put("X", 10);
+		m.put("L", 50);
+		m.put("C", 100);
+		m.put("D", 500);
+		m.put("M", 1000);
 	}
-	
-	public int getNumber( String s )
-	{
-		//complete this method
+
+	public int getNumber(String s) {
+		int total = 0;
+		int prevValue = 0;
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			String currentChar = s.substring(i, i + 1);
+			int currentValue = m.get(currentChar);
+
+			if (currentValue < prevValue) {
+				total -= currentValue;
+			} else {
+				total += currentValue;
+			}
+
+			prevValue = currentValue;
+		}
+
+		return total;
 	}
 
 }
